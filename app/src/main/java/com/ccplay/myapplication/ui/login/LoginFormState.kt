d237dc08b167da5e4ef8e371ae163ba9531f9f97
cc <cc_xie@gmail.com>
2022-03-09 18:38:08 +0800

    Initial commit 可登入後跳轉（未驗證）

---
+++ app/src/main/java/com/ccplay/myapplication/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.ccplay.myapplication.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState (val usernameError: Int? = null,
+                      val passwordError: Int? = null,
+                      val isDataValid: Boolean = false)
